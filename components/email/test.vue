1d377de506a49d0cf66aa1b572818c4c1b6c9c7e
krizis <monkriz1991@gmail.com>
2023-09-13 11:44:37 +0300

    kollection_bag

---
+++ components/email/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <e-html lang="en">
+    <e-text>test mail</e-text>
+
+    <e-hr />
+    <e-button href="https://example.com">Click me</e-button>
+  </e-html>
+</template>
+
+<script setup>
+import { EHtml, EText, EHr, EButton } from "vue-email";
+defineProps({
+  title: String,
+});
+</script>
